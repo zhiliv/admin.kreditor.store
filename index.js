72d9f8323dd5591d5c9270899dfaaa9062989ef8
Ivan Zhilnikov <35070562+zhiliv@users.noreply.github.com>
2019-04-28 20:21:54 +0300

    initial

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 var express = require('express'),
 	app = express(),
-	server = require('http').Server(app),
+  server = require('http').Server(app),
+  bodyParser = require('body-parser'),
 	clc = require('cli-color');
-	//socket = require('./modules/lib/eventsSocket');
+//socket = require('./modules/lib/eventsSocket');
 
 var tokenLeads = 'a2a43051c137da3cfc698f80b10176b0'; //токен Leads.su
 
@@ -13,23 +14,25 @@ var SettingServer = {};
 SettingServer.Adedress = '127.0.0.1';
 SettingServer.Port = 80;
 /* ---------- */
-
+app.set('view engine', 'pug'); //подключение шаблонизатора
 app.use('/public', express.static('public'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+/* главная страница */
+app.get('/', async (req, rs) => {
+		rs.render('index', {
+		});
 });
 
 try {
 	server.listen(SettingServer.Port, SettingServer.Adedress, () => {
-    console.log(
-      clc.yellow(
-        'Сервер запущен по адресу',
-        'http://' + SettingServer.Adedress+
-        ':'+
-        SettingServer.Port
-      )
-    );
+		console.log(
+			clc.yellow(
+				'Сервер запущен по адресу',
+				'http://' + SettingServer.Adedress + ':' + SettingServer.Port
+			)
+		);
 		//socket.Events(server);
 	});
 } catch (err) {
